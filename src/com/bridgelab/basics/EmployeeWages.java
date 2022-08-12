99cf0f6ede0f9587dd392d4516b027e18f180662
Nishit <nishitranjan41@gmail.com>
2022-08-12 22:16:06 +0530

    This commit is for calculation of dailyWages

--- src/com/bridgelab/basics/EmployeeWages.java
+++ src/com/bridgelab/basics/EmployeeWages.java
@@ -3,13 +3,16 @@ package com.bridgelab.basics;
 public class EmployeeWages {
     public static final int IS_Present = 1;
     public static  final int IS_AbSENT = 0;
+    static int wagePerHour = 20;
+    static int fullDayHour = 8;
     public static void main(String[] args)
     {
         System.out.println("welcome to Employee Wages");
         int attendenceCheck = (int)(Math.random()*10%2);
         if( attendenceCheck == 1 )
         {
-           System.out.println("Employee is Present");
+            System.out.println("Employee is Present");
+            System.out.println("Daily Employee Wage"+ wagePerHour*fullDayHour);
         }
         else {
            System.out.println("Employee is absent");
