a35d5a6ea8e96cf5c6a0d542fa7df9126db055f1
Nishit <nishitranjan41@gmail.com>
2022-08-12 22:33:57 +0530

    This commit is for calculation of dailyWages for PartTimeEmployees

--- src/com/bridgelab/basics/EmployeeWages.java
+++ src/com/bridgelab/basics/EmployeeWages.java
@@ -1,21 +1,27 @@
 package com.bridgelab.basics;
 
 public class EmployeeWages {
-    public static final int IS_Present = 1;
-    public static  final int IS_AbSENT = 0;
-    static int wagePerHour = 20;
-    static int fullDayHour = 8;
-    public static void main(String[] args)
-    {
-        System.out.println("welcome to Employee Wages");
-        int attendenceCheck = (int)(Math.random()*10%2);
-        if( attendenceCheck == 1 )
-        {
-            System.out.println("Employee is Present");
+         public static final int IS_PRESENT = 1;
+         public static final int IS_ABSENT = 0;
+         public static final int IS_PARTTIME = 2;
+         static int wagePerHour = 20;
+         static int fullDayHour = 8;
+         static int partTimeHour = 4;
+         public static void main(String[] args){
+         System.out.println("welcome to Employee Wages");
+         int attendenceCheck = (int)(Math.random()*10%3);
+         if( attendenceCheck == IS_PRESENT )
+         {
+            System.out.println("Employee is Present for fullday");
             System.out.println("Daily Employee Wage"+ wagePerHour*fullDayHour);
-        }
-        else {
+         }
+         else if (attendenceCheck == IS_PARTTIME) {
+            System.out.println("Employee is Part time");
+            System.out.println("part time wage is =>"+" "+partTimeHour*wagePerHour);
+
+         }
+         else {
            System.out.println("Employee is absent");
-        }
-    }
+         }
+     }
 }
