8f99c33ef762fce8adcd2d9471683b7973bd0b79
Nishit <nishitranjan41@gmail.com>
2022-08-12 22:08:03 +0530

    This commit is for AttendenceCheck

--- src/com/bridgelab/basics/EmployeeWages.java
+++ src/com/bridgelab/basics/EmployeeWages.java
@@ -1,7 +1,18 @@
 package com.bridgelab.basics;
 
 public class EmployeeWages {
-    public static void main(String[] args) {
+    public static final int IS_Present = 1;
+    public static  final int IS_AbSENT = 0;
+    public static void main(String[] args)
+    {
         System.out.println("welcome to Employee Wages");
+        int attendenceCheck = (int)(Math.random()*10%2);
+        if( attendenceCheck == 1 )
+        {
+           System.out.println("Employee is Present");
+        }
+        else {
+           System.out.println("Employee is absent");
+        }
     }
 }
