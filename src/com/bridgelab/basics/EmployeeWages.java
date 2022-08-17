d150168e843e1a71df261d48096d89a896534ecc
Nishit <nishitranjan41@gmail.com>
2022-08-17 16:29:20 +0530

    this is uc_7_refactor_to_classmethods_classvariables

--- src/com/bridgelab/basics/EmployeeWages.java
+++ src/com/bridgelab/basics/EmployeeWages.java
@@ -6,49 +6,55 @@ public class EmployeeWages {
     public static int wagePerHour = 20;
     public static int fullDayHour = 8;
     public static int partTimeHour = 4;
-    public static int monthlyWagesAsFullTime = 0;
-    public static int monthlyWagesPartTime = 0;
-    public static int dailyEmployeeWages;
-    public static int workingHour=0;
-    static int count;
+    public static int monthlyWages = 0;
 
-    public static void main(String[] args) {
+    public static int dailyEmployeeWages;
+    public static int workingHour = 0;
+    public static void calculationOfWages() {
         System.out.println("welcome to Employee Wages");
         int day = 1;
-        while (day <= 20 && workingHour<=100 )
-        {
+        while (day <= 20 && workingHour <= 100) {
             int attendenceCheck = (int) Math.floor((Math.random() * 10)) % 3;
-            switch (attendenceCheck)
-            {
+            switch (attendenceCheck) {
                 case IS_PRESENT:
-                    System.out.println("days Of month at which employee is present => " + day);
+                    System.out.println("The day at which employee is present in this month => " + day);
                     System.out.println("Employee is Present for fullday");
                     System.out.println("Daily Employee Wage" + wagePerHour * fullDayHour);
                     dailyEmployeeWages = wagePerHour * fullDayHour;
-                    monthlyWagesAsFullTime = monthlyWagesAsFullTime + dailyEmployeeWages;
-                    workingHour = workingHour+8;
+                    monthlyWages = monthlyWages + dailyEmployeeWages;
+                    workingHour = workingHour + fullDayHour;
                     day++;
-                    System.out.println("workingHour =>"+" "+workingHour);
-                    System.out.println("monthly wages of employee =>" + monthlyWagesAsFullTime);
+                    System.out.println("Cummulative workingHour =>" + " " + workingHour);
+                    System.out.println("monthly wages of employee =>" + monthlyWages);
+                    System.out.println("                                                         ");
                     break;
                 case IS_PARTTIME:
                     System.out.println("Employee is Present as Part time");
                     dailyEmployeeWages = wagePerHour * partTimeHour;
-                    monthlyWagesPartTime = monthlyWagesPartTime + dailyEmployeeWages;
-                    System.out.println("days Of month(Employee is Present) =>" + day);
+                    monthlyWages = monthlyWages + dailyEmployeeWages;
+                    System.out.println("the day at which employee is present in this month =>" + day);
                     day++;
-                    workingHour = workingHour+4;
-                    System.out.println("WorkingHour is"+" "+workingHour);
+                    workingHour = workingHour + partTimeHour;
+                    System.out.println("Cummulative WorkingHour is" + " " + workingHour);
                     System.out.println("part time wage is of one day=>" + " " + partTimeHour * wagePerHour);
-                    System.out.println("monthly wages of employee =>" + monthlyWagesPartTime);
+                    System.out.println("monthly wages of employee =>" + monthlyWages);
+                    System.out.println("                                                          ");
                     break;
                 default:
                     System.out.println("Employee is absent on day =>" + day);
                     day++;
+                    System.out.println("                                   ");
+                    break;
             }
         }
-        int totalWagesForMonth = monthlyWagesPartTime+monthlyWagesAsFullTime;
-        System.out.println("totalWagesForMonth=>"+totalWagesForMonth);
-        System.out.println("workingHour =>"+" "+workingHour);
+        {
+            int totalWagesForMonth = monthlyWages ;
+            System.out.println("totalWagesForMonth=>" + totalWagesForMonth);
+            System.out.println("workingHour =>" + " " + workingHour);
+        }
+    }
+
+    public static void main(String[] args) {
+        calculationOfWages();
     }
 }
